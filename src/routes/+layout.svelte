1ccb62d7d8006229384a18ccd037a272419734a7
Builder <92662457+Builder256@users.noreply.github.com>
2025-10-16 17:25:10 +0900

    UIを更新

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
 
 	let { children } = $props();
 </script>
@@ -12,3 +13,35 @@
 <main class="mx-auto max-w-7xl">
 	{@render children?.()}
 </main>
+
+<footer class="w-full bg-zinc-700 py-6">
+	<div class="mx-auto flex max-w-7xl flex-col gap-6 px-4">
+		<span class="text-xl font-bold">MQO Utilities</span>
+		<Separator></Separator>
+		<div class="flex flex-col justify-between sm:flex-row">
+			<span>&copy; 2025 Builder. All Rights Reserved.</span>
+			<span>
+				This site is built with
+				<a
+					href="https://github.com/sveltejs/svelte"
+					class="text-indigo-300 underline hover:no-underline"
+					target="_blank"
+					rel="noopener">Svelte</a
+				>,
+				<a
+					href="https://github.com/sveltejs/kit"
+					class="text-indigo-300 underline hover:no-underline"
+					target="_blank"
+					rel="noopener">SvelteKit</a
+				>
+				and
+				<a
+					href="https://shadcn-svelte.com/"
+					class="text-indigo-300 underline hover:no-underline"
+					target="_blank"
+					rel="noopener">shadcn-svelte</a
+				>.
+			</span>
+		</div>
+	</div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Tabs from '$lib/components/ui/tabs/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
 
 	import MQOExtractor from '../MQOExtractor';
 	const objectExtractor = new MQOExtractor();
@@ -20,41 +21,60 @@
 		const textDecoder = new TextDecoder('shift-jis');
 		const mqoText = textDecoder.decode(arrayBuffer);
 
-		const result = objectExtractor.extractObject(mqoText);
-
-		text = result.join('\n');
+		text = objectExtractor.extractObjectList(mqoText).join('\n');
 	}
 </script>
 
-<div class="my-8 grid grid-cols-12 gap-6">
+<div class="my-6 grid grid-cols-12 gap-6 px-4">
 	<div class="col-span-12 flex flex-col gap-8">
 		<hgroup class="text-center">
 			<h1 class="text-5xl font-bold">MQO Utilities</h1>
 			<p class="mt-4">MQOファイルの加工に便利なユーティリティソフト</p>
 		</hgroup>
 
-		<h2 class="text-2xl font-bold">まずはMQOファイルをアップロードしよう</h2>
-		<div class="col-span-12 grid w-full max-w-sm items-center gap-4">
-			<Label for="file">MQOファイルをアップロード</Label>
-			<Input id="file" type="file" accept=".mqo" bind:files={file} />
-		</div>
+		<Separator class="col-span-12" />
+
+		<Card.Root class="col-span-12">
+			<Card.Header>
+				<Card.Title>
+					<h2 class="text-2xl font-bold">MQOファイルをアップロード</h2>
+				</Card.Title>
+				<Card.Description>
+					<span class=""> 加工するMQOファイルをアップロードする </span>
+				</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<div class="col-span-12 grid w-full max-w-sm items-center gap-4">
+					<Label for="file">MQOファイルをアップロード</Label>
+					<Input id="file" type="file" accept=".mqo" bind:files={file} />
+				</div>
+			</Card.Content>
+		</Card.Root>
 	</div>
-	<Separator class="col-span-12" />
-	<hgroup class="col-span-12">
-		<h2 class="text-2xl font-bold">オブジェクト名一覧</h2>
-		<p class="mt-4">MQOファイルに含まれるオブジェクト名の一覧</p>
-	</hgroup>
 
-	<Button onclick={extractObjects}>抽出</Button>
-
-	<Tabs.Root value="account" class="col-span-12 w-full">
-		<Tabs.List>
-			<Tabs.Trigger value="plaintext">プレーンテキスト</Tabs.Trigger>
-			<Tabs.Trigger value="structured-view">構造化ビュー</Tabs.Trigger>
-		</Tabs.List>
-		<Tabs.Content value="plaintext">
-			<Textarea bind:value={text} class="h-[60vh] w-full" />
-		</Tabs.Content>
-		<Tabs.Content value="structured-view">まだないよ</Tabs.Content>
-	</Tabs.Root>
+	<Card.Root class="col-span-12">
+		<Card.Header>
+			<Card.Title>
+				<h2 class="text-2xl font-bold">オブジェクト名一覧</h2>
+			</Card.Title>
+			<Card.Description>
+				<span class="">MQOファイルに含まれるオブジェクト名の一覧</span>
+			</Card.Description>
+			<Card.Action>
+				<Button onclick={extractObjects}>抽出</Button>
+			</Card.Action>
+		</Card.Header>
+		<Card.Content class="flex flex-col gap-4">
+			<Tabs.Root value="plaintext" class="col-span-12 w-full">
+				<Tabs.List>
+					<Tabs.Trigger value="plaintext">プレーンテキスト</Tabs.Trigger>
+					<Tabs.Trigger value="structured-view">構造化ビュー</Tabs.Trigger>
+				</Tabs.List>
+				<Tabs.Content value="plaintext">
+					<Textarea bind:value={text} class="h-64 w-full" />
+				</Tabs.Content>
+				<Tabs.Content value="structured-view">まだないよ</Tabs.Content>
+			</Tabs.Root>
+		</Card.Content>
+	</Card.Root>
 </div>
