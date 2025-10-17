c8e0852e55a9e6e48ad217f50c31b8c5103ecb57
Builder <92662457+Builder256@users.noreply.github.com>
2025-10-17 11:32:51 +0900

    構造化ビューの折り畳み機能を実装

--- src/routes/FileTree.svelte
+++ src/routes/FileTree.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
+	import ChevronDownIcon from '@lucide/svelte/icons/chevron-down';
 	import ChevronRightIcon from '@lucide/svelte/icons/chevron-right';
 	import FileTree from './FileTree.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import { slide } from 'svelte/transition';
 	let { item: obj, nested } = $props();
+	let expandChild = $state(true);
 	const nest = new Array(nested);
 	const hasChildObj = obj.childObjects.length > 0;
+
+	function toggleExpansion() {
+		expandChild = !expandChild;
+	}
 </script>
 
 <div class="">
@@ -18,8 +25,12 @@
 		{/each}
 		{#if hasChildObj}
 			<div class="grid size-12 place-items-center">
-				<Button variant="outline" size="icon-sm">
-					<ChevronRightIcon></ChevronRightIcon>
+				<Button variant="outline" size="icon-sm" onclick={toggleExpansion}>
+					{#if expandChild}
+						<ChevronDownIcon></ChevronDownIcon>
+					{:else}
+						<ChevronRightIcon></ChevronRightIcon>
+					{/if}
 				</Button>
 			</div>
 		{:else}
@@ -30,9 +41,11 @@
 		</div>
 	</div>
 
-	<div class="">
-		{#each obj.childObjects as childObject}
-			<FileTree item={childObject} nested={nested + 1}></FileTree>
-		{/each}
-	</div>
+	{#if hasChildObj && expandChild}
+		<div class="" transition:slide={{ duration: 150 }}>
+			{#each obj.childObjects as childObject}
+				<FileTree item={childObject} nested={nested + 1}></FileTree>
+			{/each}
+		</div>
+	{/if}
 </div>
